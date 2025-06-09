acfa066a84c294dec08ec00e189b2da7cacfb9de
= <viniipvp@gmail.com>
2025-06-09 17:08:09 -0300

    Convert heic to png working

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from PIL import Image, ImageDraw
-im = Image.open(r"test.png")
-# The code opens an image file named "test.png" using the PIL library and displays it as a new temporary file.
-converted = ImageDraw.Draw(im)
-converted.text((50, 50), "As a JPG file", fill=(0, 0, 0))
-im = im.convert("RGB")
-im.save("test-converted.jpg", "JPEG")
-im.show()
+from pillow_heif import register_heif_opener
+# Register HEIF opener to handle HEIC files
 
+register_heif_opener()
+
+im = Image.open(r"TEST.HEIC")
+im.save("test-converted.png", "PNG")
